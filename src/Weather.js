c88c2c9145d45eabd062d5dfd508129a68752c72
Svitlana <106338548+fotinia-sadovskaya@users.noreply.github.com>
2023-03-20 13:55:21 +0200

    Update Weather.js

--- src/Weather.js
+++ src/Weather.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
+import { ColorRing } from "react-loader-spinner";
 import axios from "axios";
 import "./Weather.css";
 import WeatherIcon from "./WeatherIcon";
@@ -98,9 +99,15 @@ export default class Weather extends Component {
       );
     } else {
       return (
-        <div>
-          App is loading, <em>please wait...</em>
-        </div>
+        <ColorRing
+          visible={true}
+          height="80"
+          width="80"
+          ariaLabel="blocks-loading"
+          wrapperStyle={{}}
+          wrapperClass="blocks-wrapper"
+          colors={["#e15b64", "#f47e60", "#f8b26a", "#abbd81", "#849b87"]}
+        />
       );
     }
   }
