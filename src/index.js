1ac6f4f55559da18f9cf28f955e6a3cca2cd7e14
Svitlana <106338548+fotinia-sadovskaya@users.noreply.github.com>
2023-03-20 13:37:27 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDom.render(
+ReactDOM.render(
   <BrowserRouter>
     <App />
   </BrowserRouter>,
